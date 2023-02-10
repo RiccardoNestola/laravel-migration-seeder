ac9cb378d3e70dd9d2524e03e35eda032bd44be0
RiccardoNestola <riccardinux@gmail.com>
2023-02-10 18:52:23 +0100

    add limit

--- app/Http/Controllers/Guest/TrainController.php
+++ app/Http/Controllers/Guest/TrainController.php
@@ -10,7 +10,7 @@ class TrainController extends Controller
 {
     public function index() {
 
-        $trains = Train::all();
+        $trains = Train::limit(81)->get();
          /* dd($trains);  */
 
         return view('home', compact("trains"));
