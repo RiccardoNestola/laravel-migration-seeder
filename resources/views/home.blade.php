8b7e2f6d01443741bda51848564f1e574403efa2
RiccardoNestola <riccardinux@gmail.com>
2023-02-11 14:11:52 +0100

    add layout, table, css  and @if

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,54 +1,54 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layouts.app')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel</title>
+    @section('main-content')
 
-    <!-- Fonts -->
-    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
 
-    <!-- Styles -->
-    @vite('resources/js/app.js')
+    <div class="container-fluid px-2">
 
-</head>
-
-<body>
-
-    <main class="bg-light">
-        <div class="container">
-            <div class="row">
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>Compagnia</th>
+                    <th>Stazione di partenza</th>
+                    <th>Stazione di arrivo</th>
+                    <th>Orario di partenza</th>
+                    <th>Orario di arrivo</th>
+                    <th>Numero di carrozze</th>
+                    <th>Ritardo</th>
+                    <th>Cancellato</th>
+                    <th>Codice treno</th>
+                </tr>
+            </thead>
+            <tbody>
                 @foreach ( $trains as $train )
-                  
-                
-                <div class="col-12 col-md-6 col-lg-4 py-4">
-                    <div class="card text-center">
-                        <div class="card-header">
-                            Company: {{ $train->company}}
-                        </div>
-                        <div class="card-body">
-                          <h5 class="card-title text-start">Departure Station: {{ $train->departure_station}}</h5>
-                          <p class="card-text text-start">Arrival Station: {{ $train->arrival_station}}</p>
-                          <p class="card-text text-start">Departure time: {{ $train->departure_time}}</p>
-                          <p class="card-text text-start">Arrival time: {{ $train->arrival_time}}</p>
-                          <p class="card-text text-start">Number of carriages: {{ $train->number_of_carriages}}</p>
-                          <p class="card-text text-start">Delay: {{ $train->in_time}}</p>
-                          <p class="card-text text-start">Deleted: {{ $train->is_delete}}</p>
-                          <a href="#" class="btn btn-primary">BUY TICKET</a>
-                        </div>
-                        <div class="card-footer text-muted">
-                          CODE Train: {{ $train->train_code}}
-                        </div>
-                      </div>
-                </div>
+                    <tr>
+                        <td>{{ $train->company}}</td>
+                        <td>{{ $train->departure_station}}</td>
+                        <td>{{ $train->arrival_station}}</td>
+                        <td>{{ $train->departure_time}}</td>
+                        <td>{{ $train->arrival_time}}</td>
+                        <td>{{ $train->number_of_carriages}}</td>
+                        <td>
+                            @if ( $train->in_time == 1)
+                            Si
+                            @elseif ( $train->in_time == 0)
+                            No
+                            @endif
+                        </td>
+                        <td>
+                            @if ( $train->is_delete == 1)
+                            Si
+                            @elseif ( $train->is_delete == 0)
+                            No
+                            @endif
+                        </td>
+                        <td>{{ $train->train_code}}</td>
+                    </tr>
                 @endforeach
-                
-            </div>
-        </div>
-    </main>
+            </tbody>
+        </table>
+    </div>
 
-</body>
 
-</html>
+    @endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,12 @@
+<header>
+    <nav class="navbar text-bg-light p-3">
+        <div class="container-fluid">
+          <a class="navbar-brand text-white" href="#">
+            <img src="https://upload.wikimedia.org/wikipedia/commons/6/61/Logo_Ferrovie_dello_Stato_Italiane.png" alt="Logo" width="100" height="44" class="d-inline-block align-text-top">
+          </a>
+        </div>
+      </nav>
+      <h1 class="text-center pt-4">Dettagli partenze</h1>
+    
+</header>
+
